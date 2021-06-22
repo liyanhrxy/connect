881d7de637e5d41a002342de16541c87574038fe
zhao.zhang <loatheb.zhao@gmail.com>
2021-06-22 14:37:37 +0800

    fix: defectiveBatchDevices add se_ver detect

--- src/js/utils/findDefectiveBatchDevice.js
+++ src/js/utils/findDefectiveBatchDevice.js
@@ -4,9 +4,9 @@ import type {Features} from '../types';
 
 export const findDefectiveBatchDevice = (features: Features) => {
     if (!features) return;
-    const onekeySerial = features.onekey_serial;
+    const { onekey_serial: onekeySerial, se_ver: seVer } = features;
     if (!onekeySerial) return;
     const versionNum = +onekeySerial.slice(5);
     if (Number.isNaN(versionNum)) return;
-    return versionNum >= 21032200001 && versionNum <= 21032201500;
+    return (versionNum >= 21032200001 && versionNum <= 21032201500) && seVer === '1.1.0.2';
 };
