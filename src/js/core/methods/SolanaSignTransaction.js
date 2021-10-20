60a2718c8bf07ac6736d568f34f13a20cb601158
zhao.zhang <loatheb.zhao@gmail.com>
2021-10-20 15:30:05 +0800

    feat: add `solanaSignTransaction` multiple payload support.

--- src/js/core/methods/SolanaSignTransaction.js
+++ src/js/core/methods/SolanaSignTransaction.js
@@ -9,37 +9,68 @@ import { validatePath } from '../../utils/pathUtils';
 import type { CoreMessage } from '../../types';
 import type { MessageType } from '../../types/trezor/protobuf';
 
+type Params = $ElementType<MessageType, 'SolanaSignTx'>;
+type Response = {
+    signature: string;
+}
 export default class SolanaSignTransaction extends AbstractMethod {
-    params: $ElementType<MessageType, 'SolanaSignTx'>;
+    params: Params[] = [];
+    hasBundle: boolean;
+    progress: number = 0;
 
     constructor(message: CoreMessage) {
         super(message);
         this.requiredPermissions = ['read', 'write'];
-        this.info = 'Sign SOL transaction';
 
-        const { payload } = message;
+        this.hasBundle = Object.prototype.hasOwnProperty.call(message.payload, 'bundle');
+        const payload = !this.hasBundle ? { ...message.payload, bundle: [ message.payload ] } : message.payload;
 
-        // validate incoming parameters
         validateParams(payload, [
-            { name: 'path', obligatory: true },
-            { name: 'rawTx', type: 'string', obligatory: true },
+            { name: 'bundle', type: 'array' },
         ]);
 
-        const path = validatePath(payload.path, 3);
+        if (this.params.length === 1) {
+            this.info = 'Sign SOL transaction';
+        } else {
+            this.info = 'Sign multiple SOL transaction';
+        }
 
-        this.params = {
-            address_n: path,
-            raw_tx: base58_to_binary(payload.rawTx),
-        };
+        payload.bundle.forEach(batch => {
+            validateParams(batch, [
+                { name: 'path', obligatory: true },
+                { name: 'rawTx', type: 'string', obligatory: true },
+            ]);
+
+            const path = validatePath(batch.path, 3);
+
+            this.params.push({
+                address_n: path,
+                raw_tx: base58_to_binary(batch.rawTx),
+            });
+        });
     }
 
-    async run() {
+    async _call(params: Params) {
         const cmd = this.device.getCommands();
-        const { message } = await cmd.typedCall('SolanaSignTx', 'SolanaSignedTx', this.params);
+        const { message } = await cmd.typedCall('SolanaSignTx', 'SolanaSignedTx', params);
         const signature = binary_to_base58(Buffer.from(message.signature, 'hex'));
         return {
             ...message,
             signature,
         };
+    }
+
+    async run() {
+        const responses: Response[] = [];
+
+        for (let i = 0; i < this.params.length; i++) {
+            const batch = this.params[i];
+
+            const resp = await this._call(batch);
+            responses.push(resp);
+
+            this.progress++;
+        }
+        return this.hasBundle ? responses : responses[0];
     }
 }
